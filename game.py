b9b219412c3b8f40d510ce9e7488518df219f568
GusyLebedy <gusevskaya.arina@mail.ru>
2022-05-07 20:33:01 +0300

    game frame

---
+++ game.py
@@ -0,0 +1,22 @@
+class Character:
+    def __init__(self, score, amount, characteristic=None):
+        self.score = score
+        self.amount = amount
+        self.characteristic = characteristic
+
+    def score(self):
+        pass
+
+    def characteristic(self):
+        pass
+
+    def amount(self):
+        pass
+
+
+class Trophy(Character):
+    def score(self):
+        pass
+
+    def characteristic(self):
+        pass
